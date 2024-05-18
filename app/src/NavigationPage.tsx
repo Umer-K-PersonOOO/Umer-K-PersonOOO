e6d3ade5a560c0b416916c12b3aa4d89bbd1766f
PersonOOO <umerkhan5110@gmail.com>
2024-05-17 21:11:14 -0500

    part 1 of nav screen

--- app/src/NavigationPage.tsx
+++ app/src/NavigationPage.tsx
@@ -4,20 +4,54 @@ import "./NavigationPage.css";
 
 function NavigationPage() {
   return (
-    <div>
-      <div className=" h-screen bg-fallback flex items-center text-white font-serif">
-        {" "}
-        {/* Menu is not centered */}
+    <div className="relative h-screen bg-fallback text-white font-serif">
+      <div className="relative z-10 flex items-center h-full ">
         <div
           id="link-container"
-          className="group text-8xl flex flex-col gap-y-10 "
+          className="group text-6xl flex flex-col "
         >
-          <a href="/" id="links" className="group-hover:opacity-30 group-hover:hover:opacity-100 transition-opacity"> About Me </a>
-          <a href="/" id="links" className="group-hover:opacity-30 group-hover:hover:opacity-100 transition-opacity"> Experience </a>
-          <a href="/" id="links" className="group-hover:opacity-30 group-hover:hover:opacity-100 transition-opacity"> Projects </a>
-          <a href="/" id="links" className="group-hover:opacity-30 group-hover:hover:opacity-100 transition-opacity"> Contact Me </a>
+
+          <a
+            href="/"
+            id="links-about"
+            className="group-hover:opacity-30 group-hover:hover:opacity-100 transition-opacity py-6"
+          >
+            About Me
+          </a>
+          <a
+            href="/"
+            id="links-exp"
+            className="group-hover:opacity-30 group-hover:hover:opacity-100 transition-opacity py-6"
+          >
+            Experience
+          </a>
+          <a
+            href="/"
+            id="links-proj"
+            className="group-hover:opacity-30 group-hover:hover:opacity-100 transition-opacity py-6"
+          >
+            Projects
+          </a>
+          <a
+            href="/"
+            id="links-contact"
+            className="group-hover:opacity-30 group-hover:hover:opacity-100 transition-opacity py-6"
+          >
+            Contact Me
+          </a>
         </div>
       </div>
+      <div
+        className="absolute inset-0 z-0 w-[50%] h-full " //
+        id="circle-tile"
+      ></div>
+      <div
+        className="absolute inset-0 z-1 w-[30%] h-full bg-cover bg-center"
+        style={{ backgroundImage: `url('/pfptransparent.png')`, backgroundSize: "70%", backgroundRepeat: 'no-repeat'}}
+        id="background-image"
+        
+      ></div>
+      
     </div>
   );
 }
